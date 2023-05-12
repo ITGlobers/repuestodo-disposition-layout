59178d7f1ed249aa46726bb4899880fb43d49eac
Marlon De La Roche <marlondelaroch3@gmail.com>
2023-05-12 18:29:47 -0500

    working again

--- react/index.tsx
+++ react/index.tsx
@@ -5,34 +5,53 @@ import { NoSSR } from 'vtex.render-runtime'
 type Disposition = {
   order: number
   show: boolean
+  value: string
+  __editorItemTitle: string
 }
 
+type BlockProps = {
+  blockProps: { id: string }
+}
 interface Props {
-  children: ReactNode
-  disposition: Disposition[]
-  ssr?: boolean
+  children: React.ReactNode;
+  disposition: Disposition[];
+  ssr?: boolean;
+  block: "string"
 }
 
 const DispositionLayout: any = ({
   children,
   disposition,
+  block
 }: Props): ReactChild[] | ReactNode => {
+  if (block) {
+
+
+
+  }
   const array = React.Children.toArray(children) as ReactChild[]
 
   const nestedChildren =
     array.length === 1 ? (array[0] as ReactElement).props.children : null
+  const orderDispositionFix = disposition.map((e, index) => {
+    e.order = index + 1
+    return e
+  })
 
-  const sortedChildren = disposition?.reduce((acc, item, i) => {
-    const { order, show } = item
+  const sortedChildren = [] as ReactChild[]
 
+  for (let i = 0; i < orderDispositionFix.length; i++) {
+    const { order, show, value } = orderDispositionFix[i]
     if (!show) {
-      return acc
+      continue
     }
 
-    acc.splice(order - 1, 0, nestedChildren ? nestedChildren[i] : array[i])
+    const element = array.find((e) => React.isValidElement(e) && (e.props as BlockProps).blockProps.id === value)
 
-    return acc
-  }, [] as ReactChild[])
+    const child = nestedChildren ? nestedChildren[i] : element as ReactElement
+
+    sortedChildren[order - 1] = child
+  }
 
   if (sortedChildren.length <= 0) {
     return children
